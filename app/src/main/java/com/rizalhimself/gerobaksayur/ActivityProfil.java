c45f6a83d0d2c4e1990ccb8ae763218b63cbb22d
Nizar120699 <ahmadibnu722@gmail.com>
2021-07-12 22:56:46 +0700

    Menambahkan button cari barang dan profil pada mainactivitu.

---
+++ app/src/main/java/com/rizalhimself/gerobaksayur/ActivityProfil.java
@@ -0,0 +1,14 @@
+package com.rizalhimself.gerobaksayur;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityProfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil);
+    }
+}
