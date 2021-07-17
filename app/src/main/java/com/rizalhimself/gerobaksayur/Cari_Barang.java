79444953dbb1c6cbbc611820f0e9b7a93bf039a4
rizalhimself <sniperinstink@hotmail.com>
2021-07-17 19:46:50 +0700

    menambahkan activity_caribarang.xml dan activity_caribarang_2 untuk resource recyclerview

--- app/src/main/java/com/rizalhimself/gerobaksayur/Cari_Barang.java
+++ app/src/main/java/com/rizalhimself/gerobaksayur/Cari_Barang.java
@@ -9,6 +9,6 @@ public class Cari_Barang extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cari__barang);
+        setContentView(R.layout.activity_caribarang);
     }
 }
